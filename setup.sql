4e4383abadbe792acbfb107afc6e0a628c7810f0
Mauricio Rojas Fernandez <mauricio.rojas@snowflake.com>
2025-12-03 20:28:12 -0600

    deleting some files

--- setup.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE NETWORK RULE service_now_network_rule
-  TYPE = 'HOST_PORT'
-  MODE= 'EGRESS'
-  VALUE_LIST = ('*.service-now.com:443','*.service-now.com');
-
-CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION service_now_apis_access_integration
-  ALLOWED_NETWORK_RULES = (service_now_network_rule)
-  ENABLED = true;
